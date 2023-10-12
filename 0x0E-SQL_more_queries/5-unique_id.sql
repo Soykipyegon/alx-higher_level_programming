ce341f9b9e48e6853c4004c19bc08523cbd3d63b
Soykipyegon <97885831+Soykipyegon@users.noreply.github.com>
2023-10-12 08:42:48 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,7 +0,0 @@
--- This script creates the table unique_id on your MySQL server.
-
--- creating table unique id
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT UNIQUE DEFAULT 1,
-    name VARCHAR(256)
-);
